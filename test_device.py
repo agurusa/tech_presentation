44356894f3c0042d0f0bfe18741806972e416e63
Aarthi Gurusami <aarthi.gurusami@gmail.com>
2020-05-03 17:21:14 -0700

    remove time delay

--- test_device.py
+++ test_device.py
@@ -1,15 +1,10 @@
-import pytest
-import datetime
-from time import sleep
-import statistics
 from threading import Thread
-
-import simulation as sim
 import specs
 
 RPM = 200  # standard RPM acquired by shop drill
-TEST_TIME = 10  # seconds to run the drill
+TEST_TIME = 1  # seconds to run the drill
 POW_SUCCESS = 20  # must generate at least this much power to be acceptable
+
 
 def test_on(hydrogen):
     assert hydrogen.pow_con == specs.POWER_CONSUMED
@@ -17,9 +12,8 @@ def test_on(hydrogen):
 
 def test_flashed(hydrogen):
     assert hydrogen.can_address == specs.CAN_ADDRESS
+    # assert hydrogen.firmware_version == specs.FIRMWARE_VERSION
     assert hydrogen.firmware_version == specs.VERSION_PROP[hydrogen.manufacture_version]
-
-
 
 def test_LEDs(hydrogen):
     LEDs = hydrogen.get_LEDs()  # solicit the user
@@ -28,21 +22,10 @@ def test_LEDs(hydrogen):
 
 
 def test_pow_generated(hydrogen):
-    now = datetime.datetime.now()
-    end = now + datetime.timedelta(0, TEST_TIME)
     thread = start_drill(hydrogen)  # user begins spinning the generator
-    power_readings = []
-    delay = 1  # seconds
-    while now < end:
-        sleep(delay)
-        power_readings += [p.power for p in hydrogen.get_pow(delay)]
-        now = datetime.datetime.now()
     thread.join()
-    if sim.EXCEPT_POW in power_readings:
-        assert pytest.fail(sim.EXCEPT_POW)
-    else:
-        av_pow = statistics.mean(power_readings)
-        assert (av_pow >= POW_SUCCESS)
+    av_pow = hydrogen.get_pow(-1)[0].power
+    assert (av_pow >= POW_SUCCESS)
 
 
 def start_drill(_hydrogen):
@@ -52,13 +35,7 @@ def start_drill(_hydrogen):
 
 
 def run_drill(_hydrogen):
-    now = datetime.datetime.now()
-    end = now + datetime.timedelta(0, TEST_TIME)
-    delay = 0.05  # hydrogen records at 20 Hz
-    while now < end:
-        _hydrogen.generate(RPM)
-        sleep(delay)
-        now = datetime.datetime.now()
+    _hydrogen.generate(RPM)
 
 
 def test_off(hydrogen):
