bb72a4f40c62bb6f21c22ab19fa8dfae862c9c78
Aarthi Gurusami <aarthi.gurusami@gmail.com>
2020-05-04 17:49:35 -0700

    run all tests at once

--- conftest.py
+++ conftest.py
@@ -1,49 +1,78 @@
 import specs
 from simulation import HydroGen
+import copy
+import pytest
 
 NUM_SAMPLES = 100
 sample_pop = [HydroGen() for i in range(NUM_SAMPLES)]
+testversions = [0, 1, 2, 3]
+testdays = [specs.MONDAY, specs.FRIDAY]
+VERSION = 'testversion'
+DAY='testday'
 
+def setup():
+    all = []
+    for t in testversions:
+        all += [(copy.deepcopy(device), {VERSION: t, DAY: specs.MONDAY}) for device in sample_pop]
+        all += [(copy.deepcopy(device), {VERSION: t, DAY: specs.FRIDAY}) for device in sample_pop]
+    return all
 
-def setup(DUT, version, weekday):
-    v = int(version)
-
-    if v > 0:
-        DUT.fix_board()
-
-    if v > 1:
-        DUT.battery.turn_on(specs.MAX_BATT_LEVEL)
-    else:
-        DUT.battery.turn_on(weekday)
-
-    if v > 2:
-        DUT.flash(firmware_change=True)
-    else:
-        DUT.flash()
-
+def MVP(DUT, weekday):
+    DUT.battery.turn_on(weekday)
+    DUT.flash()
     DUT.turn_on()
     return DUT
 
+def VERSION1(DUT, weekday):
+    DUT.fix_board()
+    DUT.battery.turn_on(weekday)
+    DUT.flash()
+    DUT.turn_on()
+    return DUT
 
-def devices(testver, testround):
-    weekday = specs.FRIDAY if testround == '1' else specs.MONDAY
-    return [setup(d, testver, weekday) for d in sample_pop]
+def VERSION2(DUT):
+    DUT.fix_board()
+    DUT.battery.turn_on(specs.MAX_BATT_LEVEL)
+    DUT.flash()
+    DUT.turn_on()
+    return DUT
+
+def VERSION3(DUT):
+    DUT.fix_board()
+    DUT.battery.turn_on(specs.MAX_BATT_LEVEL)
+    DUT.flash(firmware_change=True)
+    DUT.turn_on()
+    return DUT
+
+@pytest.fixture(scope="module", params=setup())
+def hydrogen(request):
+    DUT = request.param[0]
+    info = request.param[1]
+    if info[VERSION] == 0:
+        MVP(DUT, info[DAY])
+    elif info[VERSION] == 1:
+        VERSION1(DUT, info[DAY])
+    elif info[VERSION] == 2:
+        VERSION2(DUT)
+    else:
+        VERSION3(DUT)
+    return DUT
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--testver",
-        choices=['0', '1', '2', '3'],
-        help='The version of the test you want to check'
-    )
-    parser.addoption(
-        "--testround",
-        choices=['1', '2'],
-        help='The round of testing you are on'
-    )
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--testver",
+#         choices=['0', '1', '2', '3'],
+#         help='The version of the test you want to check'
+#     )
+#     parser.addoption(
+#         "--testround",
+#         choices=['1', '2'],
+#         help='The round of testing you are on'
+#     )
 
 
-def pytest_generate_tests(metafunc):
-    if 'hydrogen' in metafunc.fixturenames:
-        metafunc.parametrize('hydrogen', devices(metafunc.config.getoption('testver'),
-                                                 metafunc.config.getoption('testround')), scope='module')
+# def pytest_generate_tests(metafunc):
+#     if 'hydrogen' in metafunc.fixturenames:
+#         metafunc.parametrize('hydrogen', devices(metafunc.config.getoption('testver'),
+#                                                  metafunc.config.getoption('testround')), scope='module')
